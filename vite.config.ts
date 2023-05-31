f5b1dd6a601c9668fcf2ed590ff6ab0aceea9e0c
smy-101 <shimy1011@foxmail.com>
2023-05-31 23:24:59 +0800

    feat: 安装lint相关插件,引入svgr
    feat: update dependency,install svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
+import viteSvgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteSvgr(),react()],
+  resolve:{
+    alias: {
+      // 设置public目录的别名为@public
+      '@assets': path.join(__dirname, 'src/assets')
+    }
+  }
 })
