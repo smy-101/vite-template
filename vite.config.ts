378f91a149f62f3eb9ce2f5ea529dbf42917f886
smy-101 <824860484@qq.com>
2023-06-02 09:50:28 +0800

    feat: 细化eslint设置,添加css reset
    feat: 细化设置

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,15 @@ export default defineConfig({
   resolve:{
     alias: {
       // 设置public目录的别名为@public
-      '@assets': path.join(__dirname, 'src/assets')
+      '@assets': path.join(__dirname, 'src/assets'),
+      '@': path.join(__dirname, 'src')
     }
+  },
+  build:{
+    target: 'modules', // 浏览器兼容目标,modules为默认值
+    outDir: 'dist', // 打包输出路径，dist为默认值
+    assetsDir: 'assets', // 静态资源存放路径,指定生成静态资源的存放路径（相对于 build.outDir）,在 库模式 下不能使用，assets为默认值
+    cssCodeSplit: true, // 允许 css 代码拆分，true为默认值
+    sourcemap: false, // 不生成 sourceMap 文件，false为默认值
   }
 })
